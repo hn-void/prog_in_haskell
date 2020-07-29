58fbbb2c1cb7fc7c39cca9247e40a3398383923a
hn_void <panic4thuy@gmail.com>
2020-07-29 12:43:26 -0700

    :sparkles: Add whole answers except for 5.7

--- chap_5/answers.hs
+++ chap_5/answers.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 -- 5.1
 -- n = sum [x^2 | x <- [1..100]]
 
@@ -21,3 +23,45 @@ pyths n = [(x, y, z) | x <- [1..n],
                        y <- [1..n],
                        z <- [1..n],
                        x^2 + y^2 == z^2]
+
+-- 5.6
+factors :: Int -> [Int]
+factors n = [x | x <- [1..n], n `mod` x == 0]
+
+perfects :: Int -> [Int]
+perfects n = [x | x <- [1..n], sum (factors x) - x == x]
+
+-- 5.7 not confident
+-- [(x, y) | x <- [1,2,3], y <- [4,5,6]]
+-- [(1,4),(1,5),(1,6),(2,4),(2,5),(2,6),(3,4),(3,5),(3,6)]
+-- [[(1,4),(1,5),(1,6)],[(2,4),(2,5),(2,6)],[(3,4),(3,5),(3.6)]]?
+l = [x | x <- [1,2,3]] ++ [y | y <- [4,5,6]]
+
+-- 5.8
+find :: Eq a => a -> [(a,b)] -> [b]
+find k t = [v | (k',v) <- t, k == k']
+
+positions :: Eq a => a -> [a] -> [Int]
+positions x xs = find x (zip xs [0..])
+
+-- 5.9
+scalarproduct :: [Int] -> [Int] -> Int
+scalarproduct xs ys = sum [x*y | (x, y) <- zip xs ys]
+
+-- 5.10
+let2int :: Char -> Int
+let2int c = ord c - ord 'a'
+
+cap2int :: Char -> Int
+cap2int c = ord c - ord 'A'
+
+int2let :: Int -> Char
+int2let n = chr (ord 'a' + n)
+
+int2cap :: Int -> Char
+int2cap n = chr (ord 'A' + n)
+
+shift :: Int -> Char -> Char
+shift n c | isLower c = int2let ((let2int c + n) `mod` 26)
+          | isUpper c = int2cap ((cap2int c + n) `mod` 26)
+          | otherwise = c
