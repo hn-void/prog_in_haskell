1752541c63c2ee02cd949f29619e635f8bd8bb08
hn_void <panic4thuy@gmail.com>
2020-07-28 19:25:44 -0700

    :sparkles: Add an answer of 4.5

--- chap_4/answers.hs
+++ chap_4/answers.hs
@@ -1,16 +1,16 @@
--- 4.8.1
+-- 4.1
 halve :: [a] -> ([a], [a])
 {- halve xs = (take (length xs `div` 2) xs, drop (length xs `div` 2) xs) -}
 halve xs = (take n xs, drop n xs)
            where n = length xs `div` 2
 
--- 4.8.2
+-- 4.2
 third :: [a] -> a
 {- third xs = head (tail (tail xs)) -}
 {- third xs = xs !! 2 -}
 third (_:_:x:_) = x
 
--- 4.8.3
+-- 4.3
 safetail :: [a] -> [a]
 {- safetail xs = if null xs then [] else tail xs -}
 {-
@@ -20,22 +20,30 @@ safetail xs | null xs = []
 safetail [] = []
 safetail (_:xs) = xs
 
--- 4.8.4
+-- 4.4
 {-
 True || True = True
 True || False = True
 False || True = True
 False || False = False
--}
-{-
+
 False || False = False
 _ || _ = True
--}
-{-
+
 False || b = b
 True || _ = True
--}
-{-
+
 b || c | b == c = b
        | otherwise True
+-}
+
+-- 4.5
+(&&) :: Bool -> Bool -> Bool
+a && b = if a then
+              if b then True
+                   else False
+              else False
+{-
+*Main> True Main.&& True
+True
 -}
