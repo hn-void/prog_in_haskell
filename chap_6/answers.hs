ac480ddb251468b3a9a61295f60ec5bd346e3d0c
hn_void <panic4thuy@gmail.com>
2020-07-30 22:47:25 -0700

    :sparkles: Add all answers of chapter 6

--- chap_6/answers.hs
+++ chap_6/answers.hs
@@ -49,3 +49,83 @@ euclid :: Int -> Int -> Int
 euclid x y | x == y = x
            | x > y = euclid (x-y) y
            | y > x = euclid x (y-x)
+
+-- 6.5
+{-
+length [1,2,3]
+= 1 + length [2,3]
+= 1 + 1 + length [3]
+= 1 + 1 + 1 + length []
+= 1 + 1 + 1 + 1
+= 4
+
+drop 3 [1,2,3,4,5]
+= drop 2 [2,3,4,5]
+= drop 1 [3,4,5]
+= drop 0 [4,5]
+= [4,5]
+
+init [1,2,3]
+= 1 : init [2,3]
+= 1 : 2 : init [3]
+= 1 : 2 : []
+= [1,2] 
+-}
+
+-- 6.6
+myAnd :: [Bool] -> Bool
+myAnd [] = True
+myAnd (x:xs) = x && myAnd xs
+
+myConcat :: [[a]] -> [a]
+myConcat [] = []
+myConcat (x:xs) = x ++ myConcat xs
+
+myReplicate :: Int -> a -> [a]
+myReplicate 0 _ = []
+myReplicate n x = x : myReplicate (n-1) x
+
+myDoubleExclamation :: [a] -> Int -> a
+myDoubleExclamation (x:xs) 0 = x
+myDoubleExclamation (x:xs) n = myDoubleExclamation xs (n-1)
+{-
+*Main> [1,2,3,4,5,6,7,8,9] `myDoubleExclamation` 3
+4
+-}
+
+myElem :: Eq a => a -> [a] -> Bool
+myElem _ [] = False
+myElem a (x:xs) = a == x || myElem a xs
+
+-- 6.7
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys) = if x < y
+                         then x : merge xs (y:ys)
+                         else y : merge (x:xs) ys
+
+-- 6.8
+halve :: [a] -> ([a], [a])
+halve xs = (take n xs, drop n xs)
+    where n = length xs `div` 2
+
+-- msort :: Ord a => a -> [a] -> [a]
+msort [] = []
+msort [x] = [x]
+msort xs = merge (msort l) (msort r)
+    where (l,r) = halve xs
+
+-- 6.9
+mySum :: Num a => [a] -> a
+mySum [] = 0
+mySum (x:xs) = x + mySum xs
+
+myTake :: Int -> [a] -> [a]
+myTake _ [] = []
+myTake 0 xs = []
+myTake n (x:xs) = x : myTake (n-1) xs
+
+myLast :: [a] -> a
+myLast [x] = x
+myLast (x:xs) = myLast xs
