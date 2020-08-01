56d5a2a88794a51ae9d84f02f987ca655ccdff5f
hn_void <panic4thuy@gmail.com>
2020-08-01 13:38:14 -0700

    :sparkles: Add answers of chapter 7 except for 7.9-

--- chap_7/answers.hs
+++ chap_7/answers.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 -- 7.1
 -- map f (filter p xs)
 
@@ -36,3 +38,76 @@ myCurry f = \x y -> f (x,y)
 
 myUncurry :: (a -> b -> c) -> ((a, b) -> c)
 myUncurry f = \(x,y) -> f x y
+
+-- 7.6
+unfold p h t x | p x = []
+               | otherwise = h x : unfold p h t (t x)
+
+type Bit = Int
+
+chop8 :: [Bit] -> [[Bit]]
+chop8 = unfold null (take 8) (drop 8)
+
+myMap2 :: (a -> a) -> [a] -> [a]
+myMap2 f = unfold null (f . head) tail
+
+myIterate :: (a -> a) -> a -> [a]
+myIterate f = unfold (\_ -> False) f f
+
+-- 7.7
+bin2int :: [Bit] -> Int
+bin2int = foldr (\x y -> x + 2*y) 0
+
+int2bin :: Int -> [Bit]
+int2bin = unfold (== 0) (`mod` 2) (`div` 2)
+
+make8 :: [Bit] -> [Bit]
+make8 bits = take 8 (bits ++ repeat 0)
+
+encode :: String -> [Bit]
+encode = concat . map (make8 . int2bin . ord)
+
+{-
+chop8 :: [Bit] -> [[Bit]]
+chop8 = unfold null (take 8) (drop 8)
+-}
+
+decode :: [Bit] -> String
+decode = map (chr . bin2int) . chop8
+
+transmit :: String -> String
+transmit = decode . channel . encode
+
+channel :: [Bit] -> [Bit]
+channel = id
+
+count :: Eq a => a -> [a] -> Int
+count x = length . filter (== x)
+
+addParity :: [Bit] -> [Bit]
+addParity bits = (if odd (count 1 bits) then 1 else 0) : bits
+
+checkParity :: [Bit] -> [Bit]
+checkParity bits = if even (count 1 bits) then (tail bits) else error "Parity Error"
+
+normalTransmit :: String -> String
+normalTransmit = decode . addParity . channel . checkParity . encode
+
+errorChannel :: [Bit] -> [Bit]
+errorChannel = tail
+
+errorTransmit :: String -> String
+errorTransmit = decode . addParity . errorChannel . checkParity . encode
+
+{-
+If the error channel drops "1", then the count of "1" changes and is detected as an error. 
+However, if the channel drops "0", the count of "1" doesn't change so that it cannot be detected.
+
+*Main> errorTransmit "higher-functions are easy"
+"*** Exception: Parity Error
+CallStack (from HasCallStack):
+  error, called at answers.hs:91:65 in main:Main
+
+*Main> errorTransmit "higher-functions are difficult"
+"\180\180\&3\180\&2\185\SYN\179:\183\&1\186\180\&7\183\&9\144\&0\185\&2\DLE\178\&43\179\180\177:6:"
+-}
