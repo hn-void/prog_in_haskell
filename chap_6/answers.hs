8bb154ce1a2f01b5b5c44e2b6d834a306297fab4
hn_void <panic4thuy@gmail.com>
2020-08-01 14:04:55 -0700

    :bug: Fix a wrong type signiture

--- chap_6/answers.hs
+++ chap_6/answers.hs
@@ -110,7 +110,7 @@ halve :: [a] -> ([a], [a])
 halve xs = (take n xs, drop n xs)
     where n = length xs `div` 2
 
--- msort :: Ord a => a -> [a] -> [a]
+msort :: Ord a => [a] -> [a]
 msort [] = []
 msort [x] = [x]
 msort xs = merge (msort l) (msort r)
